61bc388d9e7eca4401b2f606c251cc8eb1fde032
rikutan <163800982+rikutan@users.noreply.github.com>
2024-10-17 21:38:46 +0900

    add アプリタイトル表示テキストの追加

--- app/src/main/java/com/example/udemypractice2/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice2/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.udemypractice2.ui.theme.UdemyPractice2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UdemyPractice2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "BMI計算アプリ",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UdemyPractice2Preview() {
     UdemyPractice2Theme {
-        Greeting("Android")
+        MainScreen()
     }
 }
