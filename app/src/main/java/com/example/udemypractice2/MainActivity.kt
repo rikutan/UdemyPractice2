0ed28cdde7ed97dc9670939da78f508606841069
rikutan <163800982+rikutan@users.noreply.github.com>
2024-10-17 22:26:59 +0900

    add 身長テキストフィールドの追加

--- app/src/main/java/com/example/udemypractice2/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice2/MainActivity.kt
@@ -5,14 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,7 +52,35 @@ fun MainScreen() {
         Text(
             text = "BMI計算アプリ",
             fontSize = 26.sp,
-            fontWeight = FontWeight.ExtraBold
+            fontWeight = FontWeight.ExtraBold,
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // 身長
+        Text(
+            text = "身長(cm)",
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+        )
+
+        // 入力欄、value=入力欄に表示する文字列、onValueChange=入力欄に値が入力されたときの処理を担当
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            colors = TextFieldDefaults.colors(
+
+                // テキストフィールドの背景色を変更、Transparent は、透明化する
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent
+            ),
+            placeholder = { Text(text = "170") },
+
+            // キーボードでの入力を数字にする
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+
+            // テキストフィールドの入力表示を制御する trueなら一行、falseなら複数行、入力可能
+            singleLine = true,
         )
     }
 }
